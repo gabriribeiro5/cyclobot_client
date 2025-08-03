c93bc5059c4117464cfe1f07d825095581007e90
Gabriel Book360 <gabriribeiro5@live.com>
2025-08-03 19:13:31 -0300

    fix(warning): library ArduinoUniqueID may be incompatible with your current board which runs on megaavr architecture(s).   - [x] Add support for both ArduinoUniqueID and megaavr with conditional compilation using architecture macros

--- include/config/DeviceParameters.h
+++ include/config/DeviceParameters.h
@@ -1,6 +1,14 @@
 #pragma once
-#include <ArduinoUniqueID.h>
-#include <SoftwareSerial.h>
+// For ATmega4809
+#if defined(__AVR_ATmega4809__)
+  #include <stdint.h>
+  #include <string.h>
+  #define USE_MEGAAVR_ID
+#elif defined(ARDUINO_ARCH_MEGAAVR)
+  #define USE_MEGAAVR_ID
+#else
+  #include <ArduinoUniqueID.h>
+#endif
 
 class DeviceParameters {
 public:
@@ -16,10 +24,24 @@ public:
 
 private:
     String getUniqueId() {
+    #ifdef USE_MEGAAVR_ID
+        // For ATmega4809: read from SIGROW
+        const uint16_t sigRowAddr = 0x1100;
+        String id = "";
+        for (uint8_t i = 0; i < 10; i++) {
+            uint8_t b = *((uint8_t *)(sigRowAddr + i));
+            if (b < 0x10) id += '0';
+            id += String(b, HEX);
+        }
+        return id;
+    #else
+        // Use ArduinoUniqueID library
         String id = "";
         for (size_t i = 0; i < UniqueIDsize; i++) {
+            if (UniqueID[i] < 0x10) id += '0';
             id += String(UniqueID[i], HEX);
         }
         return id;
+    #endif
     }
 };
