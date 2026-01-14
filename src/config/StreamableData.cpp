a573e5121f53364f5c68e7cc13c2a530dd655f35
Gabriel Book360 <gabriribeiro5@live.com>
2026-01-14 05:24:41 -0300

    Revert "feat(stream_sensors_data): streaming with frequency control"
    This reverts commit ffe36e123fd09324659fd8a96bc44b15d6f8ed05.

--- src/config/StreamableData.cpp
+++ src/config/StreamableData.cpp
@@ -13,156 +13,39 @@ StreamableData::StreamableData()
     sensor_ints = LinkedList<Sensor_Int>();
 };
 
-/************************ SEARCH METHODS ************************/
-// SETUP
-StreamableData::Setup_Bool StreamableData::setup_bool(char *name)
-{
-    for (int i = 0; i < sizeof(setup_bool); i++)
-    {
-        Setup_Bool item = setup_bools.get(i);
-        if (item.name == name)
-        {
-            return item;
-        };
-    };
-    return;
-};
-StreamableData::Setup_Int StreamableData::setup_int(char *name)
-{
-    for (int i = 0; i < sizeof(setup_int); i++)
-    {
-        Setup_Int item = setup_ints.get(i);
-        if (item.name == name)
-        {
-            return item;
-        }
-    }
-    return;
-};
-StreamableData::Setup_Uint8_t StreamableData::setup_uint8_t(char *name)
-{
-    for (int i = 0; i < sizeof(setup_uint8_t); i++)
-    {
-        Setup_Uint8_t item = setup_uint8_ts.get(i);
-        if (item.name == name)
-        {
-            return item;
-        }
-    }
-    return;
-};
-// SENSOR
-StreamableData::Sensor_Bool StreamableData::sensor_bool(char *name)
-{
-    for (int i = 0; i < sizeof(sensor_bool); i++)
-    {
-        Sensor_Bool item = sensor_bools.get(i);
-        if (item.name == name)
-        {
-            return item;
-        }
-    };
-    return;
-};
-StreamableData::Sensor_Int StreamableData::sensor_int(char *name)
-{
-    for (int i = 0; i < sizeof(sensor_int); i++)
-    {
-        Sensor_Int item = sensor_ints.get(i);
-        if (item.name == name)
-        {
-            return item;
-        }
-    };
-    return;
-};
-
-/************************ INSERT METHODS ************************/
-// SETUP
-void StreamableData::add_setup_param(char* type,
-                                char *name,
-                                bool value,
-                                char *description,
-                                bool is_pin_value,
-                                bool user_can_see,
-                                bool updated_by,
-                                DateTime last_update
-                            )
-{
+void StreamableData::setup_add(char* type, char *name, bool value, char *description, bool is_pin_value, bool user_can_see, bool updated_by, DateTime last_update) {
     if (type == "bool") {
         Setup_Bool new_bool = {
-            name = name,
-            value = value,
-            description = description,                 // data description (for final user - not admin or server)
-            is_pin_value = is_pin_value,               // default = false
-            user_can_see = user_can_see,               // default = true
-            updated_by = updated_by,                   // default = 0 (server communication); 1 = (user communication)
-            last_update = last_update
+            name,
+            value,
+            description,                 // data description (for final user - not admin or server)
+            is_pin_value,                 // default = false
+            user_can_see,                 // default = true
+            updated_by,                   // default = 0 (server communication); 1 = (user communication)
+            last_update
         };
-        setup_bools.add(new_bool);
     };
     if (type == "int") {
         Setup_Int new_int = {
-            name = name,
-            value = value,
-            description = description,                 // data description (for final user - not admin or server)
-            is_pin_value = is_pin_value,               // default = false
-            user_can_see = user_can_see,               // default = true
-            updated_by = updated_by,                   // default = 0 (server communication); 1 = (user communication)
-            last_update = last_update
+            
         };
         setup_ints.add(new_int);
-    };
+    }
     if (type == "uint8_t") {
-        Setup_Uint8_t new_uint8_t = {
-            name = name,
-            value = value,
-            description = description,                 // data description (for final user - not admin or server)
-            is_pin_value = is_pin_value,               // default = false
-            user_can_see = user_can_see,               // default = true
-            updated_by = updated_by,                   // default = 0 (server communication); 1 = (user communication)
-            last_update = last_update
-        };
-        setup_uint8_ts.add(new_uint8_t);
-    };
+        return;
+    }
+    // soilIsWet(0),
+    // soilMoistureLimit(500),
+    // currentTemperature(20),
+    // minTemperatureExpected(18),
+    // maxTemperatureExpected(22),
+    // initialWateringTimeLimit(2000),
+    // growthRate(0),
+    // decreaseRate(0),
+    // wateringTimeLimit(3000),
+    // climate(1),
+    // sunLightAvailable(1),
+    // standBy(0),
+    // timeBufferMicroSec(1800000000)
 };
-
-/************************ INSERT METHODS ************************/
-// SENSOR
-void StreamableData::add_sensor_param(char* type,
-                                char *name,
-                                int value,
-                                char *description,
-                                int expected_frequency,
-                                bool measure_now,
-                                bool send_now,
-                                DateTime last_update
-                            )
-{
-    if (type == "bool") {
-        Sensor_Bool new_bool = {
-            name = name,
-            value = value,
-            description = description,                 // data description (for final user - not admin or server)
-            expected_frequency = expected_frequency,   // used by stream_sensors_data();
-            measure_now = measure_now,                 // used by stream_sensors_data();
-            send_now = send_now,                       // used by stream_sensors_data();
-            last_update = last_update
-        };
-        sensor_bools.add(new_bool);
-    };
-    return;
-    if (type == "int") {
-        Sensor_Int new_int = {
-            name = name,
-            value = value,
-            description = description,                 // data description (for final user - not admin or server)
-            expected_frequency = expected_frequency,   // used by stream_sensors_data();
-            measure_now = measure_now,                 // used by stream_sensors_data();
-            send_now = send_now,                       // used by stream_sensors_data();
-            last_update = last_update
-        };
-        sensor_ints.add(new_int);
-    };
-    return;
-};
+void StreamableData::sensor_add(char* type, char *name, int value, char *description, int expected_frequency, bool measure_now, bool send_now, DateTime last_update);
